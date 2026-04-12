5575e7c9e29ea54a75bcaaf11ff4bcb52d54ee23
Mesbah-M13 <mesbah0010@gmail.com>
2026-04-12 07:05:01 +0600

    problem 2 solved

---
+++ Week_2/02_multiples.c
@@ -0,0 +1,57 @@
+/*
+Given two numbers A and B. Print "Multiples" if A is multiple of B or vice versa. Otherwise print "No Multiples".
+
+Input
+Only one line containing two numbers A, B (1  ≤  A, B  ≤  106)
+
+Output
+Print the "Multiples" or "No Multiples" corresponding to the read numbers.
+
+Examples
+InputCopy
+9 3
+OutputCopy
+Multiples
+InputCopy
+6 24
+OutputCopy
+Multiples
+InputCopy
+12 5
+OutputCopy
+No Multiples
+Note
+***A is said to be Multiple of B if B is divisible by A.
+
+First Example :
+
+9 is divisible by 3 , So the answer is: Multiples.
+
+Second Example :
+
+6 is not divisible by 24 but
+
+24 is divisible by 6 , So the answer is: Multiples.
+
+Third Example :
+
+12 is not divisible by 5 and 5 is not divisible by 12.
+
+So the answer is: No Multiples.
+*/
+
+#include<stdio.h>
+
+int main(){
+     
+    int a,b=0;
+    scanf("%d %d", &a , &b);
+
+    if(a%b==0 || b%a == 0){
+        printf("Multiples");
+    }else{
+        printf("No Multiples");
+    }
+    
+    return 0;
+}
