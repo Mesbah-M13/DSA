14ee7823860f9e8065cb5c244c205513e28f9361
Mesbah-M13 <mesbah0010@gmail.com>
2026-04-13 06:19:27 +0600

    problem 4 solved other way

--- Week_2/04_char.c
+++ Week_2/04_char.c
@@ -1,3 +1,30 @@
+/*
+
+Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
+
+Note : difference between 'a' and 'A' in ASCII is 32 .
+
+Input
+Only one line containing a character X which will be a capital or small letter.
+
+Output
+Print the answer to this problem.
+
+Examples
+InputCopy
+a
+OutputCopy
+A
+InputCopy
+A
+OutputCopy
+a
+
+*/
+
+
+/* ✅ Own approach - worked */
+/* 
 #include<stdio.h>
 
 int main(){
@@ -14,5 +41,24 @@ int main(){
         printf("%c", a);
     }
 
+    return 0;
+}
+    */
+
+
+#include<stdio.h>
+
+int main(){
+     char ch = 'a';
+     scanf("%c",&ch);
+
+     if( ch>='a' && ch<='z'){
+        ch -= 32;
+        printf("%c", ch);
+     }
+     else{
+        ch += 32;
+        printf("%c", ch);
+     }
     return 0;
 }
