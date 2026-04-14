0d2abb3774a572cf111cb11ea332ac585babae8e
Mesbah-M13 <mesbah0010@gmail.com>
2026-04-14 16:45:53 +0600

    Problem 7 solved

---
+++ Week_2/Module_06(Using Loop)/01_even_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    if (n == 1)
+    {
+        printf("-1");
+    }
+    
+    else if (n > 1)
+    {
+        for (int i = 2; i <= n; i++)
+        {   
+            if (i % 2 == 0)
+            {
+                printf("%d\n", i);
+            }
+            
+        }
+    }
+
+    return 0;
+}
