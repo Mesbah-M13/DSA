5582a1fd706664dd6d4dfb25c1f6bbe899487cc0
Mesbah-M13 <mesbah0010@gmail.com>
2026-04-16 05:51:22 +0600

    Problem 11 solved own way

---
+++ Week_2/Module_06(Using Loop)/F_Multiplication_table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+     int n = 0;
+     scanf("%d", &n);
+
+     for (int i = 1; i <= 12; i++)
+     {
+        int res=0;
+        res = i * n;
+        printf("%d * %d = %d\n", n , i , res);
+     }
+     
+    return 0;
+}
