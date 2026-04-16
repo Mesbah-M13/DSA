c15ed95cf63c9ea573815f888c0646915f236cbb
Mesbah-M13 <mesbah0010@gmail.com>
2026-04-16 21:45:11 +0600

    Problem 13 solved

--- Week_2/Module_06(Using Loop)/Q_Digits.c
+++ Week_2/Module_06(Using Loop)/Q_Digits.c
@@ -1,31 +1,56 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    int n = 0;
-    scanf("%d", &n);
-    int x = 0;
+// int main()
+// {
+//     int n = 0;
+//     scanf("%d", &n);
+//     int x = 0;
 
-    for (int i = 0; i < n; i++)
-    {
+//     for (int i = 0; i < n; i++)
+//     {
 
-        scanf("%d", &x);
-        if( x == 0){
-            printf("0");
-        }
-        while (x != 0)
-        {
-            printf("%d ", x % 10);
-            x = x / 10;
-        }
-        printf("\n");
-    }
+//         scanf("%d", &x);
+//         if( x == 0){
+//             printf("0");
+//         }
+//         while (x != 0)
+//         {
+//             printf("%d ", x % 10);
+//             x = x / 10;
+//         }
+//         printf("\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 /*
 1200
 
 
 */
+
+#include<stdio.h>
+
+int main(){
+    
+    int n = 0;
+    scanf("%d", &n);
+    int num = 0;
+
+    for(int i = 1; i<= n ; i++){
+        scanf("%d", &num);
+
+        if( num == 0){
+            printf("0");
+        }
+
+        while(num != 0){
+            printf("%d ",num % 10);
+            num /=  10;
+        }
+        printf("\n");  
+    }
+
+    return 0;
+}
