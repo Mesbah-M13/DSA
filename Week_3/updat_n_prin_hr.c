561e81f3ffa0412acc45e5fd43132a316ffc7c11
Mesbah-M13 <mesbah0010@gmail.com>
2026-04-20 16:52:59 +0600

    update & print ( bit complecated HR)

---
+++ Week_3/updat_n_prin_hr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 0;
+    scanf("%d ", &n);
+    int arr[n];
+    int x = 0 , y =0; 
+    
+    for( int i = 0 ; i < n ; i++){
+        scanf("%d ", &arr[i]);
+    }
+
+    scanf("%d %d", &x, &y);
+    arr[x] = y;
+    
+     for( int i = n-1 ; i >= 0 ; i--){
+        printf("%d ", arr[i]);
+    }
+       
+    return 0;
+}
