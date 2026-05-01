67145cb16fb9f6095b8e363e4aa2ea4105b0a30f
Mesbah-M13 <mesbah0010@gmail.com>
2026-05-01 22:17:51 +0600

    Problem 33 solved ( array )

---
+++ Week_2/Module_07(Array)/expp/E_Lowest_Number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+       scanf("%d", &a[i]);
+    }
+    
+    int min = INT_MAX;
+    int idx;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] < min){
+            min = a[i];
+            idx = i;
+        }
+    }
+    printf("%d %d", min, idx+1);
+    
+
+    
+    
+    return 0;
+}
